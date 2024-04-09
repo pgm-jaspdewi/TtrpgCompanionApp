6732194b8837e6bb92f14f9021ed348447a26c02
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-04-09 17:37:36 +0200

    Installed tailwind and set up custom colors for project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'maroon': '#800000',
+      'silverChalice': '#B0B8B8',
+      'darkKhaki': '#C0AD6A',
+      'lightKaki': '#F8F2D6',
+      'paleGold': '#EAD9A8',
+      'indigo': '#4B0082',
+      'redishBrown': '#A52A2A',
+      'britishRacingGreen': '#00563F',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
