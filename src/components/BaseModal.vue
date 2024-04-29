f8e2f9e25cb37eb95b635650b0d61d00cb25ccce
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-04-29 16:05:42 +0200

    Added fade-in & -out to modal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,15 +1,22 @@
 <template>
-  <div
-    v-show="modalActive"
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8 z-20"
-  >
-    <div v-if="modalActive" class="p-4 bg-paleGold self-start mt-32 max-w-screen-tablet">
-      <slot />
-      <BaseButton btnContent="Close" @click="store.toggleStatModal">
-        <FaXmark class="fill-maroon group-hover:fill-lightKaki w-5 h-5" />
-      </BaseButton>
+  <Transition name="modal-outer">
+    <div
+      v-show="modalActive"
+      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8 z-20"
+    >
+      <Transition name="modal-inner">
+        <div
+          v-if="modalActive"
+          class="p-4 bg-paleGold self-start mt-32 max-w-screen-tablet border-2 border-darkKhaki rounded-lg"
+        >
+          <slot />
+          <BaseButton btnContent="Close" @click="store.toggleStatModal">
+            <FaXmark class="fill-maroon group-hover:fill-lightKaki w-5 h-5" />
+          </BaseButton>
+        </div>
+      </Transition>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
@@ -27,3 +34,34 @@ defineProps({
   }
 })
 </script>
+
+<!-- styling for modal transition -->
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+</style>
