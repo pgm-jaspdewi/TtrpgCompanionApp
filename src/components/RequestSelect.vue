dc7b8f167667105cb9736d8790607f763465c05b
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-05-05 16:39:57 +0200

    All different types of radioButton selections on WizardStep5.vue now work.
    They still look horrible though

---
+++ src/components/RequestSelect.vue
@@ -0,0 +1,41 @@
+<!-- 
+  This select input will first make the fetchrequest that is responisble for filling it's options.
+  This is necessary because at some places in the app the options are not known at compile time.
+  However, the url necessary for the fetch request is known at compile time.
+  Therefore, the url is passed as a prop to this component, and the fetch request is made in the setup function.
+ -->
+<template>
+  <div class="flex flex-col">
+    <select
+      class="text-sm p-1 m-2 my-1 rounded-lg outline-none px-3 border-2 border-darkKhaki bg-lightKaki focus:border-maroon focus:ring-1 focus:ring-maroon"
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLSelectElement).value)"
+    >
+      <option value="" disabled :selected="'' === modelValue">Select an option</option>
+      <option v-for="item in options" :key="item.index" :value="item.index">
+        {{ item.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios'
+
+const props = defineProps({
+  // modelValue prop is required, it is the v-model binding
+  modelValue: {
+    type: String,
+    default: '',
+    required: true
+  },
+  url: {
+    type: String,
+    default: '',
+    required: true
+  }
+})
+
+const request = await axios.get(import.meta.env.VITE_5E_URL + props.url)
+const options = request.data.equipment
+</script>
--- src/components/WizardStep5.vue
+++ src/components/WizardStep5.vue
@@ -1,8 +1,100 @@
 <template>
-  <h2>Step 5</h2>
+  <div class="m-4">
+    <BaseLabel pl="small" :pb="true">Select equipment</BaseLabel>
+    <div class="border-2 border-darkKhaki rounded-lg p-2">
+      <p class="text-sm">
+        Apart from the standard equipment provided by your class and background, a
+        <span class="lowercase font-bold">{{ characterClass.data.name }}</span> also gets to choose
+        from the following options:
+      </p>
+      <p>{{ selectedOption }}</p>
+      <div
+        v-for="(choice, index) in radioButtonOptions"
+        :key="index"
+        class="p-2 border-2 flex flex-col"
+      >
+        <p>{{ choice.desc }}</p>
+
+        <div v-for="(option, i) in choice.from.options" :key="i">
+          <!-- display for counted reference -->
+          <label v-if="option.option_type === 'counted_reference'" class="flex">
+            <input
+              type="radio"
+              v-model="selectedOption[index]"
+              :value="{ amount: option.count, item: option.of.name }"
+            />
+            <p class="m-2 my-1">{{ option.of.name }} ( {{ option.count }} )</p>
+          </label>
+
+          <!-- display for multiple -->
+          <label v-if="option.option_type === 'multiple'" class="flex">
+            <input
+              type="radio"
+              v-model="selectedOption[index]"
+              :value="[
+                { amount: option.items[0].count, item: option.items[0].of.name },
+                { amount: option.items[1].count, item: option.items[1].of.name }
+              ]"
+            />
+            <p class="m-2 my-1">
+              {{ option.items[0].of.name }} ( {{ option.items[0].count }} ) &
+              {{ option.items[1].of.name }} ( {{ option.items[1].count }} )
+            </p>
+          </label>
+
+          <!-- display for choice -->
+          <label v-if="option.option_type === 'choice'" class="flex">
+            <p>{{ index }}</p>
+            <input
+              type="radio"
+              v-model="selectedOption[index]"
+              :value="{ amount: 1, item: itemValue[index] }"
+            />
+            <RequestSelect
+              :url="option.choice.from.equipment_category.url"
+              v-model="itemValue[index]"
+            />
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
   <WizardNav />
 </template>
 
 <script setup lang="ts">
+import axios from 'axios'
 import WizardNav from './WizardNav.vue'
+import { useWizardStore } from '@/stores/wizard-store'
+import BaseLabel from './BaseLabel.vue'
+import { ref } from 'vue'
+import RequestSelect from './RequestSelect.vue'
+
+const store = useWizardStore()
+
+const itemValue = ref([''])
+console.log(itemValue.value)
+
+const characterClass = await axios.get(
+  import.meta.env.VITE_5E_API_URL + 'classes/' + store.characterInfo.charClass
+)
+// get the equipment options for the class
+const equipmentOptions = characterClass.data.starting_equipment_options
+//  loop through the options to get the type of option set. This will be used to determine how to display the options
+const radioButtonOptions = ref<any[]>([])
+const selectInputOptions = ref<any[]>([])
+for (let i = 0; i < equipmentOptions.length; i++) {
+  if (equipmentOptions[i].from.option_set_type === 'options_array') {
+    radioButtonOptions.value.push(equipmentOptions[i])
+  } else {
+    selectInputOptions.value.push(equipmentOptions[i])
+  }
+}
+
+const selectedOption = ref<string[]>([''])
+for (let i = 1; i < radioButtonOptions.value.length; i++) {
+  selectedOption.value.push('')
+}
+
+console.log(selectedOption.value)
 </script>
