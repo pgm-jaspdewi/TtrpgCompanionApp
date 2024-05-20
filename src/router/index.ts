438eef15575a752e8ac70a533fa088bde3e9c582
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-05-20 14:29:09 +0200

    Added dynamic route

--- src/router/index.ts
+++ src/router/index.ts
@@ -39,6 +39,15 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/WizardView.vue')
     },
+    {
+      // This route is a dynamic route that will match any path after /character/
+      path: '/character/:id',
+      name: 'character',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/CharacterView.vue')
+    },
 
     // This is a catch-all route in case the user enters an invalid URL
     {
