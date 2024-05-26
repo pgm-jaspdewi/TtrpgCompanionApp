8a95005e15492553354557ac32b7a1ab5392921b
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-05-26 18:52:08 +0200

    Clearly commented file because i was getting lost in my own code

--- src/components/characterPageComponents/SkillSave.vue
+++ src/components/characterPageComponents/SkillSave.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>{{ skill.name }} {{ skillModifier }}</div>
+  <div>{{ skill.name }} ({{ connectedStat }}) {{ skillModifier }}</div>
 </template>
 
 <script setup lang="ts">
 import type { savingThrows } from '@/interfaces'
 import axios from 'axios'
+import { connected } from 'process'
 
 import { computed, ref, watch } from 'vue'
-import BaseInputSmall from '../baseComponents/BaseInputSmall.vue'
 
 const props = defineProps({
   skill: {
@@ -32,39 +32,47 @@ const props = defineProps({
   }
 })
 
-// console.log(props.characterStats)
+// skill stat according to the dnd rules
 const stat = ref<number>(0)
-const bonus = ref<number>(0)
+// stat-modifier bonus according to the dnd rules
+const modifier = ref<number>(0)
+// stat connected to the displayed skill
+const connectedStat = ref<string>('')
+// check if the character is proficient in the stat
 const proficient = ref<boolean>(false)
 
-// console.log(props.characterSkills)
-
 const getSkillStat = async () => {
+  // Fetch the skill data from the API
   const response = await axios.get(import.meta.env.VITE_5E_URL + props.skill.url)
+  // Get the key of the stat connected to the skill
   const key = response.data.ability_score.index
+  // Set the connected stat
+  connectedStat.value = key
+  // Check if the character is proficient in the stat
   const savesIndexes: string[] = props.saves.map((save) => save.index)
-  // console.log(savesIndexes)
-  // console.log(key)
+  // Set whether the character is proficient in said stat
   proficient.value = savesIndexes.includes(key)
-  // console.log(proficient.value)
+  // Set the stat value
   if (key != undefined) {
     stat.value = props.characterStats[key]
   }
 }
-
 getSkillStat()
 
+// Watch the stat value and update the modifier accordingly
 watch(stat, () => {
-  bonus.value = Math.floor((stat.value - 10) / 2)
-  // console.log(bonus.value)
+  modifier.value = Math.floor((stat.value - 10) / 2)
+  // console.log(modifier.value)
 })
+
+// THE SKILL PROFICIENCY BONUS IS NOT CALCULATED YET. THIS NEEDS TO BE IMPLEMENTED TOMORROW
 
 // Calculate the modifier according to the dnd rules
 const skillModifier = computed(() => {
   if (proficient.value) {
-    return props.proficiencyBonus + bonus.value
+    return props.proficiencyBonus + modifier.value
   } else {
-    return bonus.value
+    return modifier.value
   }
 })
 </script>
