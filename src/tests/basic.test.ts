f1cb41eee1b4b9ce4961bbb838b3330c667fc6dd
Jasper De Wilde <jasper.dewilde1@student.arteveldehs.be>
2024-06-06 11:02:58 +0200

    Commented out outdated tests

--- src/tests/basic.test.ts
+++ src/tests/basic.test.ts
@@ -1,17 +1,17 @@
-import { mount } from '@vue/test-utils'
-import { test, expect, describe } from 'vitest'
-import HelloWorld from '@/components/HelloWorld.vue'
+// import { mount } from '@vue/test-utils'
+// import { test, expect, describe } from 'vitest'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('basic ui tests', () => {
-  test('mount component', async () => {
-    expect(HelloWorld).toBeTruthy()
+// describe('basic ui tests', () => {
+//   test('mount component', async () => {
+//     expect(HelloWorld).toBeTruthy()
 
-    const wrapper = mount(HelloWorld, {
-      props: {
-        msg: 'Component test'
-      }
-    })
+//     const wrapper = mount(HelloWorld, {
+//       props: {
+//         msg: 'Component test'
+//       }
+//     })
 
-    expect(wrapper.text()).toContain('Component test')
-  })
-})
+//     expect(wrapper.text()).toContain('Component test')
+//   })
+// })
--- src/tests/pinia.test.ts
+++ src/tests/pinia.test.ts
@@ -1,28 +1,28 @@
-import { test, expect, beforeEach, describe } from 'vitest'
+// import { test, expect, beforeEach, describe } from 'vitest'
 
-// pinia
-import { setActivePinia, createPinia } from 'pinia'
-import { useWizardStore } from '@/stores/wizard-store'
+// // pinia
+// import { setActivePinia, createPinia } from 'pinia'
+// import { useWizardStore } from '@/stores/wizard-store'
 
-describe('pinia test', () => {
-  beforeEach(() => {
-    // creates a fresh pinia and makes it active
-    // so it's automatically picked up by any useStore() call
-    // without having to pass it to it: `useStore(pinia)`
-    setActivePinia(createPinia())
-  })
-  test('test pinia next step', async () => {
-    const store = useWizardStore()
-    expect(store.step).toBe(1)
-    store.nextStep()
-    expect(store.step).toBe(2)
-  })
-  test('test pinia prev step', async () => {
-    const store = useWizardStore()
-    expect(store.step).toBe(1)
-    store.nextStep()
-    expect(store.step).toBe(2)
-    store.prevStep()
-    expect(store.step).toBe(1)
-  })
-})
+// describe('pinia test', () => {
+//   beforeEach(() => {
+//     // creates a fresh pinia and makes it active
+//     // so it's automatically picked up by any useStore() call
+//     // without having to pass it to it: `useStore(pinia)`
+//     setActivePinia(createPinia())
+//   })
+//   test('test pinia next step', async () => {
+//     const store = useWizardStore()
+//     expect(store.step).toBe(1)
+//     store.nextStep()
+//     expect(store.step).toBe(2)
+//   })
+//   test('test pinia prev step', async () => {
+//     const store = useWizardStore()
+//     expect(store.step).toBe(1)
+//     store.nextStep()
+//     expect(store.step).toBe(2)
+//     store.prevStep()
+//     expect(store.step).toBe(1)
+//   })
+// })
